510bf92d0f28cdef6ba7727aa3aba853f2bbdf15
sofiapaiva90 <73502292+sofiapaiva90@users.noreply.github.com>
2021-08-06 23:04:02 +0100

    Added Search

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Loader from "react-loader-spinner";
-import FormattedDate from "./FormattedDate";
+
+import WeatherDetails from "./WeatherDetails";
 
 import "./Weather.css";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
+    const [city, setCity] = useState(props.defaultCity);
+
     function handleResponse(response) {
         setWeatherData({
             ready: true,
@@ -16,41 +19,42 @@ export default function Weather(props) {
             humidity: response.data.main.humidity,
             iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
             date: new Date(response.data.dt * 1000),
-
         });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+    function handleNewCity(event) {
+        setCity(event.target.value);
+    }
+
+    function search() {
+        const apiKey = "470498c2d4e432b94c48ce9eb7ed132a";
+        let apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiURL).then(handleResponse);
     }
     if (weatherData.ready) {
         return (
             <div className="Weather">
-                <h1> <i className="fa fa-thumb-tack" aria-hidden="true"></i>{weatherData.city} </h1>
-                <h4><FormattedDate date={weatherData.date} /></h4>
-                <div className="row">
-                    <div className="col-8">
-                        <img src={weatherData.iconUrl} alt="weather icon" />
-                        <div className="degrees">{Math.round(weatherData.temperature)}</div><div className="units"><span className="celsius">°C</span>|<span className="fahrenheit">F</span></div>
-                    </div>
-                    <div className="col-4 TempDetails">
-                        <span className="humidity"><strong>Humidity:</strong> {weatherData.humidity}%</span>
-                        <span className="wind"><strong>Wind:</strong> {Math.round(weatherData.wind)}Km/h</span>
-                    </div>
-                </div>
-                <form>
+                <WeatherDetails details={weatherData} />
+                <form onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-9">
-                            <input type="city" className="form-control" placeholder="Type a city..." autoComplete="off" />
+                            <input type="city" className="form-control" placeholder="Type a city..."
+                                onChange={handleNewCity} />
                         </div>
                         <div className="col-3">
                             <button type="submit" className="btn btn-primary mb-2">Search</button>
                         </div>
                     </div>
                 </form>
-            </div>
+            </div >
         );
     }
     else {
-        const apiKey = "470498c2d4e432b94c48ce9eb7ed132a";
-        let apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-        axios.get(apiURL).then(handleResponse);
+        search();
 
         return (
             <Loader type="ThreeDots"
---
+++ src/WeatherDetails.js
@@ -0,0 +1,21 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherDetails(props) {
+    return (
+        <div className="WeatherDeatils">
+            <h1> <i className="fa fa-thumb-tack" aria-hidden="true"></i>{props.details.city} </h1>
+            <h4><FormattedDate date={props.details.date} /></h4>
+            <div className="row">
+                <div className="col-8">
+                    <img src={props.details.iconUrl} alt="weather icon" />
+                    <div className="degrees">{Math.round(props.details.temperature)}</div><div className="units"><span className="celsius">°C</span>|<span className="fahrenheit">F</span></div>
+                </div>
+                <div className="col-4 TempDetails">
+                    <span className="humidity"><strong>Humidity:</strong> {props.details.humidity}%</span>
+                    <span className="wind"><strong>Wind:</strong> {Math.round(props.details.wind)}Km/h</span>
+                </div>
+            </div>
+        </div>
+    );
+}
